ac19cc894ce5b025be61ec994a508e676538ea50
JordanRobin <jordanrobin100@gmail.com>
2024-09-12 18:53:40 +0200

    initial commit : project creation

---
+++ src/main/java/fr/eni/veterinary_clinic/VeterinaryClinicApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.veterinary_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeterinaryClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeterinaryClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/veterinary_clinic/VeterinaryClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.veterinary_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinaryClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
